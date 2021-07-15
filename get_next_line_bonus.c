31c70c3d7d20b250cbb9dd83787c498ed5ea7bec
Oriol Ventura <oventura@c1r12s5.42madrid.com>
2021-07-15 18:47:35 +0200

    GNL 100% With Bonus Functional

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
 void	gnl_read(int fd, char *buf, char **save)
 {
@@ -77,73 +77,4 @@ char	*get_next_line(int fd)
 	}
 	gnl_read(fd, buf, &save);
 	return (gnl_fill_lane(&save));
-}
-
-int main(int argc, char **argv)
-{
-	int fd;
-	int	fd2;
-	int ret;
-	char *line;
-	int	i;
-
-	line = 0;
-	if (argc == 3)
-	{
-		fd = open(argv[1], O_RDONLY);
-		i = 1;
-		while ((line = get_next_line(fd)) != 0)
-		{
-			printf("Line #%s\n", line);
-			free(line);
-		}
-		printf("Line #%s\n", line);
-		if (ret == -1)
-			printf("ERROR\n");
-		else if (ret == 0)
-			printf("EOF\n");
-		fd2 = open(argv[2], O_RDONLY);
-		close(fd);
-		fd = fd2;
-		while ((line = get_next_line(fd)) > 0)
-		{
-			printf("Line #%s\n", line);
-			free(line);
-		}
-		printf("Line #%s\n", line);
-		if (ret == -1)
-			printf("ERROR\n");
-		if (ret == 0)
-			printf("EOF\n");
-	}
-	if (argc == 2)
-	{
-		fd = open(argv[1], O_RDONLY);
-		while ((line = get_next_line(fd)) != 0)
-		{
-			printf("Line #%s\n", line);
-			free(line);
-		}
-		printf("Line #%s\n", line);
-		if (ret == -1)
-			printf("-----------\nError\n");
-		else if (ret == 0)
-			printf("-----------\nEnd of file\n");
-		close(fd);
-	}
-	if (argc == 1)
-	{
-		while ((line = get_next_line(fd)) != 0)
-		{
-			line = get_next_line(fd);
-			printf("Line #%s\n", line);
-			free(line);
-		}
-		if (ret == -1)
-			printf("-----------\nError\n");
-		else if (ret == 0)
-			printf("-----------\nEnd of stdin\n");
-		close(fd);
-	}
-	return (0);
 }
